727046665bcfdc452c1ead8d2fe4523f72597de5
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2023-05-09 18:58:51 +0000

    formatting change

--- processors/ssh_command_line.py
+++ processors/ssh_command_line.py
@@ -1,5 +1,5 @@
 """
-    FFM by @JusticeRage
+    FFM by @JusticeRage and @ice-wzl
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
